a6347b4c92687c5355b0bb3b42ba0977dd19f207
HermanLin <hl3213@nyu.edu>
2021-05-02 03:35:42 -0400

    added drawing capabilities, added timer event for updating

--- Components/ground-station/plotter.py
+++ Components/ground-station/plotter.py
@@ -1,42 +1,57 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QIcon, QPixmap, QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QThread, QRect
 
-class Window(QWidget):
+class Locator():
+    def __init__(self):
+        self.x = 41.315917
+        self.y = -74.488976
+    
+    def update(self, x, y):
+        # print('updating location:{0}. {1}'.format(x, y))
+        self.x = x
+        self.y = y
+
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Coordinate Plotter'
         self.resize(640, 480)
-    
+
+        self.locator = Locator()
+        self.coords = QLabel(self)
+        self.initStuff()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updateLocation)
+        self.timer.start(1000)
+
+    def initStuff(self):
+        self.coords.setGeometry(0, 463, 175, 15)
+        self.coords.setStyleSheet('background-color: white')
+
     def paintEvent(self, event):
         painter = QPainter(self)
         pixmap = QPixmap('map.png')
-
         painter.drawPixmap(self.rect(), pixmap)
-        painter.setPen(QPen(Qt.red, 1))
-        painter.drawEllipse(self.width // 2, self.height // 2, 5, 5)
+        painter.end()
 
-class Shape():
-    def __init__(self, length, position, color, parent=None):
-        self.color = color
-        self.position = position
-        self.color = color
-
-    def paint(self, painter):
-        pass
-
-class Circle(Shape):
-    def paint(self, painter):
-        if not painter.isActive():
-            return
-        painter.save()
-        painter.setPen(QPen(self.color, 4, Qt.SolidLine))
-        x, y = self.position.x(), self.position.y()
+        self.drawLocator(self.locator)
+        self.show()
     
+    def drawLocator(self, locator):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
+        painter.drawEllipse(locator.x, locator.y, 3, 3)
+
+    def updateLocation(self):
+        self.locator.update(self.locator.x + 0.000001, self.locator.y + 0.000001)
+        self.coords.setText('{0:2.10}, {1:2.10}'.format(self.locator.x, self.locator.y))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = Window()
+    win = MainWindow()
     win.show()
     sys.exit(app.exec_())
