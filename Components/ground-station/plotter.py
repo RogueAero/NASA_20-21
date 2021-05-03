42fc610a850fa0f8f8cf788233c59a6a8ab511a0
HermanLin <hl3213@nyu.edu>
2021-05-02 23:39:00 -0400

    implemented moving Locator

--- Components/ground-station/plotter.py
+++ Components/ground-station/plotter.py
@@ -3,10 +3,13 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QIcon, QPixmap, QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt, QTimer, QThread, QRect
 
+WIDTH = 640
+HEIGHT = 480
+
 class Locator():
     def __init__(self):
-        self.x = 41.315917
-        self.y = -74.488976
+        self.x = 41.325917
+        self.y = -74.489999
     
     def update(self, x, y):
         # print('updating location:{0}. {1}'.format(x, y))
@@ -17,15 +20,15 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Coordinate Plotter'
-        self.resize(640, 480)
+        self.resize(WIDTH, HEIGHT)
 
         self.locator = Locator()
         self.coords = QLabel(self)
         self.initStuff()
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.updateLocation)
-        self.timer.start(1000)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(0.0001)
 
     def initStuff(self):
         self.coords.setGeometry(0, 463, 175, 15)
@@ -37,13 +40,23 @@ class MainWindow(QWidget):
         painter.drawPixmap(self.rect(), pixmap)
         painter.end()
 
+        self.updateLocation()
         self.drawLocator(self.locator)
         self.show()
     
     def drawLocator(self, locator):
         painter = QPainter(self)
-        painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
-        painter.drawEllipse(locator.x, locator.y, 3, 3)
+        painter.setPen(QPen(Qt.cyan, 3, Qt.SolidLine))
+        scaled_x, scaled_y = self.translateLoc(locator.x, locator.y)
+        painter.drawEllipse(scaled_x, scaled_y, 3, 3)
+
+    def translateLoc(self, x, y):
+        # (0,0) for map is (41.315917, -74.490298)
+        # (WIDHT,HEIGHT) for map is (41.334287, -74.490298)
+
+        scaled_x = int(WIDTH * (41.334287 - x) // 0.01837)
+        scaled_y = int(HEIGHT * (74.490298 - abs(y)) // 0.001322)
+        return scaled_x, scaled_y
 
     def updateLocation(self):
         self.locator.update(self.locator.x + 0.000001, self.locator.y + 0.000001)
